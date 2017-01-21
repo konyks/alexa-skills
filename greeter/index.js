96053c65f32c7be4b1779056566f60ffa8fab604
Serhiy Konyk <konyk93@gmail.com>
2017-01-21 15:36:28 -0500

    Added help, stop, and cancel intents.

--- greeter/index.js
+++ greeter/index.js
@@ -10,9 +10,23 @@ app.launch(function(req,res){
     var prompt = 'Greeting to you! What is your name?';
 });
 
+var cancelIntentFunction = function(req, res) {
+  res.say('Goodbye!').shouldEndSession(true);
+};
+
+app.intent('AMAZON.CancelIntent', {}, cancelIntentFunction);
+
+app.intent('AMAZON.StopIntent', {}, cancelIntentFunction);
+
+app.intent('AMAZON.HelpIntent', {},
+  function(req, res) {
+    var help = 'Welcome to Greeter. To start tell me your name. You can also say stop or cancel to exit.';
+    response.say(help).shouldEndSession(false);
+  });
+
 app.intent('NameIntent', {
-    "slots":{"NAME":"LITERAL"}
-    ,"utterances":["{My name is} {bob|nick|matt|jane|NAME}"]
+    'slots':{'NAME':'LITERAL'}
+    ,'utterances':['{My name is} {bob|nick|matt|jane|NAME}']
 },function(req,res){
     res.say(getGreeting()+' '+req.slot('NAME'));
 });
