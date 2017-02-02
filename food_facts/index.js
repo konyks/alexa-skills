72c147780299cc4efe43fd8a5ce6f30b822fb348
konyks <konyk93@gmail.com>
2017-02-02 10:25:37 -0500

    Fixed a type in the cancel/stop intent. Renamed the tests class.

--- food_facts/index.js
+++ food_facts/index.js
@@ -7,20 +7,20 @@ var alexa = require('alexa-app');
 var app = new alexa.app('foodfacts');
 
 // Launch intent function
-var launchIntentFunction = function(req, res){
+var launchIntentFunction = function(req, res) {
     var prompt = 'Greeting to you! Tell me a name of food?';
     res.say(prompt).shouldEndSession(false);
 };
 
 // Help intent function
-var helpIntentFunction = function(req, res){
+var helpIntentFunction = function(req, res) {
     var help = 'To start tell me the food you would like to know nutrition facts about? You can also say stop or cancel to exit.';
     res.say(help).shouldEndSession(false);
 };
 
 // Cancel intent function
 var cancelIntentFunction = function(req, res) {
-  res.say('Goodbye! Eath health!').shouldEndSession(true);
+    res.say('Goodbye! Eath healthy!').shouldEndSession(true);
 };
 
 app.launch(launchIntentFunction);
