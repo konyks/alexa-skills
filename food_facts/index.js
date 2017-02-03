6a6bf80055b75b5099aaf31a8a5ebc1aebbdb040
Serhiy Konyk <konyk93@gmail.com>
2017-02-02 23:49:15 -0500

    Added aplitity to request data from nutritionix API and parsing it to a intent response.

--- food_facts/index.js
+++ food_facts/index.js
@@ -2,24 +2,28 @@
 
 module.change_code = 1;
 
+var _ = require('lodash');
+
 var alexa = require('alexa-app');
 
 var app = new alexa.app('foodfacts');
 
+var NutritionDataHelper = require('./nutrition_data_helper');
+
 // Launch intent function
-var launchIntentFunction = function(req, res) {
+var launchIntentFunction = function (req, res) {
     var prompt = 'Greeting to you! Tell me a name of food?';
     res.say(prompt).shouldEndSession(false);
 };
 
 // Help intent function
-var helpIntentFunction = function(req, res) {
+var helpIntentFunction = function (req, res) {
     var help = 'To start tell me the food you would like to know nutrition facts about? You can also say stop or cancel to exit.';
     res.say(help).shouldEndSession(false);
 };
 
 // Cancel intent function
-var cancelIntentFunction = function(req, res) {
+var cancelIntentFunction = function (req, res) {
     res.say('Goodbye! Eath healthy!').shouldEndSession(true);
 };
 
@@ -31,6 +35,25 @@ app.intent('AMAZON.StopIntent', {}, cancelIntentFunction);
 
 app.intent('AMAZON.HelpIntent', {}, helpIntentFunction);
 
-//TODO: create food name intent
+app.intent('FoodInfo', {
+        'slots': {
+            'FOODNAME': 'LITERAL'
+        },
+        'utterances': ['{Food is} {apple|fish|pineapple|FOODNAME}']
+    },
+    function (req, res) {
+        var nutritionHelper = new NutritionDataHelper();
+        nutritionHelper.requestNutritionData(req.slot('FOODNAME')).then(function (foodData) {
+
+            //TODO: add some re prompt logic
+
+            var data = nutritionHelper.formatNutritionData(foodData);
+            console.log(data);
+            res.say(data).shouldEndSession(false).send();
+        }).catch(function (err) {
+            console.log(err.statusCode);
+        });
+    }
+);
 
 module.exports = app;
--- food_facts/nutrition_data_helper.js
+++ food_facts/nutrition_data_helper.js
@@ -2,12 +2,49 @@
 
 var _ = require('lodash');
 
-var requestPromise = require('request-promise');
+var rp = require('request-promise');
 
-var ENDPOINT = '';
+var ENDPOINT = 'https://nutritionix-api.p.mashape.com/v1_1/search/';
+var PARAMETERS = '?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat';
+var APIKEY = ''; //Add API Key
 
-function NutritionDataHelper(){ }
+function NutritionDataHelper() {}
 
-NutritionDataHelper.prototype.requestNutritionData = function(foodName){
-    
-}
+// Request Nutrition Data Function
+NutritionDataHelper.prototype.requestNutritionData = function (foodName) {
+    return this.getNutritionData(foodName).then(
+        function (response) {
+            console.log('success - received nutrition data for ' + foodName);
+
+            return response.body;
+        }
+    );
+};
+
+//Get Nutrition Data Function
+NutritionDataHelper.prototype.getNutritionData = function (foodName) {
+    var options = {
+        method: 'GET',
+        uri: ENDPOINT + foodName + PARAMETERS,
+        headers: {
+            'X-Mashape-Key': APIKEY,
+            'Accept': 'application/json'
+        },
+        resolveWithFullResponse: true,
+        json: true
+    };
+
+    return rp(options);
+};
+
+//Format Nutrition Data
+NutritionDataHelper.prototype.formatNutritionData = function (foodData) {
+    var caloriesData = _.template('${food} has ${calories} calories.')({
+        food: foodData.hits[0].fields.item_name,
+        calories: foodData.hits[0].fields.nf_calories
+    });
+
+    return caloriesData;
+};
+
+module.exports = NutritionDataHelper;
